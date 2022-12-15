8b1df9998203c06ba432f34e29ccaa5030ffbe0c
João Mangueira <jamjs7@gmail.com>
2022-12-15 17:47:42 -0300

    pacman aparecendo no jogo

--- game.js
+++ game.js
@@ -4,10 +4,22 @@ const canvasContext = canvas.getContext("2d");
 const pacmanFrames = document.getElementById("animations");
 const ghostFrames = document.getElementById("ghosts");
 
-let createReact = (x, y, width, heigth, color) => {
+let createReact = (x, y, width, height, color) => {
     canvasContext.fillStyle = color;
-    canvasContext.fillRect(x, y, width, heigth);
+    canvasContext.fillRect(x, y, width, height);
 }
+
+let fps = 30;
+let oneBlockSize = 20;
+let wallColor = "#342DCA";
+let wallSpaceWidth = oneBlockSize / 1.5;
+let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
+let wallInnerColor = "black";
+
+const DIRECTION_RIGHT = 4;
+const DIRECTION_UP = 3;
+const DIRECTION_LEFT = 2;
+const DIRECTION_BOTTOM = 1;
 
 let map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -34,3 +46,91 @@ let map = [
     [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
+
+let gameLoop = () => {
+    update();
+    draw();
+}
+
+let update = () => {
+    pacman.moveProcess();
+}
+
+let draw = () => {
+    createReact(0,0, canvas.width, canvas.height, "black");
+    drawWalls();
+
+    pacman.draw();
+}
+
+let gameInternal = setInterval(gameLoop, 1000 / fps);
+
+let drawWalls = () => {
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[0].length; j++) {
+            if (map[i][j] == 1) {
+                //the it is a wall
+                createReact(
+                    j * oneBlockSize,
+                    i * oneBlockSize,
+                    oneBlockSize,
+                    oneBlockSize,
+                    wallColor,
+                );
+
+                if (j > 0 && map[i][j - 1] == 1) {
+                    createReact(
+                        j * oneBlockSize,
+                        i * oneBlockSize + wallOffset,
+                        wallSpaceWidth + wallOffset,
+                        wallSpaceWidth,
+                        wallInnerColor,
+                    );
+                }
+
+                if (j < map[0].length - 1 && map[i][j + 1] == 1) {
+                    createReact(
+                        j * oneBlockSize + wallOffset,
+                        i * oneBlockSize + wallOffset,
+                        wallSpaceWidth + wallOffset,
+                        wallSpaceWidth,
+                        wallInnerColor,
+                    );
+                }
+
+                if (i > 0 && map[i - 1][j] == 1) {
+                    createReact(
+                        j * oneBlockSize  + wallOffset,
+                        i * oneBlockSize,
+                        wallSpaceWidth,
+                        wallSpaceWidth + wallOffset,
+                        wallInnerColor,
+                    );
+                }
+
+                if (i < map.length - 1 && map[i + 1][j] == 1) {
+                    createReact(
+                        j * oneBlockSize + wallOffset,
+                        i * oneBlockSize + wallOffset,
+                        wallSpaceWidth,
+                        wallSpaceWidth + wallOffset,
+                        wallInnerColor,
+                    );
+                }
+            }
+        }
+    }
+}
+
+let createNewPacman = () => {
+    pacman = new Pacman(
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize / 5,
+    );
+}
+
+createNewPacman();
+gameLoop();
