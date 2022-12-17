24ae367c02b83660e5fada9634ec76f1b6cd8099
João Mangueira <jamjs7@gmail.com>
2022-12-16 21:23:46 -0300

    ghosts se movimentando

--- game.js
+++ game.js
@@ -26,10 +26,10 @@ const DIRECTION_LEFT = 2;
 const DIRECTION_BOTTOM = 1;
 
 let ghostLocations = [
-    {x:0, y:0},
-    {x:176, y:0},
-    {x:0, y:121},
-    {x:176, y:121},
+    { x:0, y:0 },
+    { x:176, y:0 },
+    { x:0, y:121 },
+    { x:176, y:121 },
 ];
 
 let map = [
@@ -58,6 +58,13 @@ let map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
+let randomTragetForGhosts = [
+    { x: 1 * oneBlockSize, y: 1 * oneBlockSize },
+    { x: 1 * oneBlockSize, y: (map.length - 2) * oneBlockSize },
+    { x: (map[0].length - 2) * oneBlockSize, y: oneBlockSize },
+    { x: (map[0].length - 2) * oneBlockSize, y: (map.length - 2) * oneBlockSize },
+];
+
 let gameLoop = () => {
     update();
     draw();
@@ -66,6 +73,9 @@ let gameLoop = () => {
 let update = () => {
     pacman.moveProcess();
     pacman.eat();
+    for (let i = 0; i < ghosts.length; i++) {
+        ghosts[i].moveProcess();
+    }
 }
 
 let drawFoods = () => {
--- ghost.js
+++ ghost.js
@@ -22,9 +22,24 @@ class Ghost {
         this.imageWidth = imageWidth;
         this.imageHeight = imageHeight;
         this.range = range;
+        this.randomTargetIndex = parseInt(Math.random() * randomTragetForGhosts.length);
+
+        setInterval(() => {
+            this.changeRandomDirection();
+        }, 10000)
+    }
+
+    changeRandomDirection() {
+        this.randomTargetIndex += 1;
+        this.randomTargetIndex = this.randomTargetIndex % 4;
     }
 
     moveProcess() {
+        if (this.isInRangeOfPacman()) {
+            this.target = pacman
+        } else {
+            this.target = randomTragetForGhosts[this.randomTargetIndex]
+        }
         this.changeDirectionIfPossible();
         this.moveForwards();
         if(this.checkCollisions()) {
@@ -85,16 +100,29 @@ class Ghost {
         return isCollided;
     }
 
-    checkGhostCollision() {
+    isInRangeOfPacman() {
+        let xDistance = Math.abs(pacman.getMapX() - this.getMapX());
+        let yDistance = Math.abs(pacman.getMapY() - this.getMapY());
+        if (Math.sqrt(xDistance * xDistance + yDistance * yDistance) <= this.range) {
+            return true;
+        }
 
+        return false;
     }
 
     changeDirectionIfPossible() {
         let tempDirection = this.direction;
         this.direction = this.calculateNewDirection(
             map,
-            parse
+            parseInt(this.target.x / oneBlockSize),
+            parseInt(this.target.y / oneBlockSize),
         );
+
+        if (typeof this.direction == "undefined") {
+            this.direction = tempDirection;
+            return;
+        }
+
         this.moveForwards();
 
         if (this.checkCollisions()) {
@@ -103,6 +131,84 @@ class Ghost {
         } else {
             this.moveBackwards();
         }
+    }
+
+    calculateNewDirection(map, destX, destY) {
+        let mp = [];
+
+        for (let i = 0; i < map.length; i++) {
+            mp[i] = map[i].slice();
+        }
+
+        let queue = [{
+            x: this.getMapX(),
+            y: this.getMapY(),
+            moves: [
+
+            ],
+        }];
+
+        while (queue.length > 0) {
+            let poped = queue.shift();
+            if (poped.x == destX && poped.y == destY) {
+                return poped.moves[0]
+            } else {
+                mp[poped.y][poped.x] = 1;
+                let neighborList = this.addNeighbors(poped, mp);
+                for (let i = 0; i < neighborList.length; i++) {
+                    queue.push(neighborList[i])
+                }
+            }
+        }
+        return DIRECTION_UP;
+    }
+
+    addNeighbors(poped, mp) {
+        let queue = [];
+        let numOfRows = mp.length;
+        let numOfColumns = map[0].length;
+
+        if (
+            poped.x - 1 >= 0 &&
+            poped.x - 1 < numOfRows &&
+            mp[poped.y][poped.x - 1] != 1
+        ) {
+            let tempMoves = poped.moves.slice();
+            tempMoves.push(DIRECTION_LEFT);
+            queue.push({ x: poped.x - 1, y: poped.y, moves: tempMoves })
+        }
+
+        if (
+            poped.x + 1 >= 0 &&
+            poped.x - 1 < numOfRows &&
+            mp[poped.y][poped.x + 1] != 1
+        ) {
+            let tempMoves = poped.moves.slice();
+            tempMoves.push(DIRECTION_RIGHT);
+            queue.push({ x: poped.x + 1, y: poped.y, moves: tempMoves })
+        }
+
+        if (
+            poped.x - 1 >= 0 &&
+            poped.x - 1 < numOfRows &&
+            mp[poped.y - 1][poped.x] != 1
+        ) {
+            let tempMoves = poped.moves.slice();
+            tempMoves.push(DIRECTION_UP);
+            queue.push({ x: poped.x, y: poped.y - 1, moves: tempMoves })
+        }
+
+        if (
+            poped.x + 1 >= 0 &&
+            poped.x + 1 < numOfRows &&
+            mp[poped.y + 1][poped.x] != 1
+        ) {
+            let tempMoves = poped.moves.slice();
+            tempMoves.push(DIRECTION_BOTTOM);
+            queue.push({ x: poped.x, y: poped.y + 1, moves: tempMoves })
+        }
+
+        return queue;
     }
 
     changeAnimation() {
